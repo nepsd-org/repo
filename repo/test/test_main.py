7aa97912902bb8e4ab747eee946e420739ca2e7c
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-07-31 16:26:31 +0300

    fix(test): link tests to test cases

--- repo/test/test_main.py
+++ repo/test/test_main.py
@@ -2,12 +2,12 @@ import pytest
 
 from ..src.main import add
 
-def test_add_1():
+def test_add_ND_T1():
     assert add(3, 4) == 7
 
-def test_add_2():
+def test_add_ND_T2():
     assert add(0.3, -1) == -0.7
 
-def test_add_3():
-    with pytest.raises(TypeError) as e_info:
+def test_add_ND_T3():
+    with pytest.raises(TypeError) as _e_info:
         assert 5 + '0' != 5
